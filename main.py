93b5c05506acd93410ea7a40193eddc0540bf2c2
jarkoacjak <videojarkohrarobloxacjak@gmail.com>
2026-07-25 10:56:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+```python
 # -*- coding: utf-8 -*-
 import sys
 import urllib.parse
 import xbmcgui
 import xbmcplugin
 
+
 def build_url(query):
     return sys.argv[0] + '?' + urllib.parse.urlencode(query)
+
 
 def main():
     handle = int(sys.argv[1])
@@ -15,14 +18,20 @@ def main():
     mode = params.get('mode')
     action = params.get('action')
 
-    # --- ŠPECIÁLNA AKCIA PRE BAMBUĽKU 3 (Vyskakovacie okno) ---
-    if action == 'bambulka3_oznam':
+    # ---------------- OZNAM PRE NEDOSTUPNÉ EPIZÓDY ----------------
+    if action == 'oznam':
         dialog = xbmcgui.Dialog()
-        dialog.notification('Bambuľka 3', 'Ešte sme nevysielali alebo neni pridane', xbmcgui.NOTIFICATION_INFO, 5000)
+        dialog.notification(
+            'Informácia',
+            'Ešte sme nevysielali alebo nie je pridané.',
+            xbmcgui.NOTIFICATION_INFO,
+            5000
+        )
         return
 
-    # --- HLAVNÉ MENU ---
+    # ---------------- HLAVNÉ MENU ----------------
     if not mode:
+
         categories = [
             {"label": "Relácie", "mode": "relacie"},
             {"label": "Filmy", "mode": "filmy"},
@@ -33,56 +42,91 @@ def main():
             li = xbmcgui.ListItem(label="[B]" + kat["label"] + "[/B]")
             url = build_url({'mode': kat['mode']})
             xbmcplugin.addDirectoryItem(handle, url, li, True)
-        
+
         xbmcplugin.endOfDirectory(handle)
 
-    # --- SEKCIA RELÁCIE (Logistika s automatickou upútavkou) ---
-    elif mode == 'relacie':
-        video_id = "_oFCqhIa9Ls"
-        url = "plugin://plugin.video.youtube/play/?video_id=" + video_id
-        
+    # ---------------- RELÁCIE ----------------
+    elif mode == "relacie":
+
         li = xbmcgui.ListItem(label="Logistika")
-        li.setInfo('video', {
-            'title': 'Logistika', 
-            'plot': 'Pripravujeme čoskoro... Po kliknutí sa spustí upútavka.'
-        })
-        li.setProperty('IsPlayable', 'true')
-        
-        xbmcplugin.addDirectoryItem(handle, url, li, False)
+        url = build_url({'mode': 'logistika'})
+        xbmcplugin.addDirectoryItem(handle, url, li, True)
+
         xbmcplugin.endOfDirectory(handle)
 
-    # --- SEKCIA DETI (Bambuľka) ---
-    elif mode == 'deti':
-        # 1. a 2. epizóda
+    # ---------------- LOGISTIKA ----------------
+    elif mode == "logistika":
+
+        li = xbmcgui.ListItem(label="1. séria")
+        url = build_url({'mode': 'logistika_s1'})
+        xbmcplugin.addDirectoryItem(handle, url, li, True)
+
+        xbmcplugin.endOfDirectory(handle)
+
+    # ---------------- LOGISTIKA 1. SÉRIA ----------------
+    elif mode == "logistika_s1":
+
+        epizody = [
+            {
+                "label": "1. epizóda",
+                "id": "JLftU2_Metg"
+            }
+        ]
+
+        for ep in epizody:
+            li = xbmcgui.ListItem(label=ep["label"])
+            li.setProperty('IsPlayable', 'true')
+
+            url = "plugin://plugin.video.youtube/play/?video_id=" + ep["id"]
+
+            xbmcplugin.addDirectoryItem(handle, url, li, False)
+
+        xbmcplugin.endOfDirectory(handle)
+
+    # ---------------- DETI ----------------
+    elif mode == "deti":
+
         episody = [
             {"label": "Bambuľka 1", "id": "UOCo8fLEoUo"},
             {"label": "Bambuľka 2", "id": "674vZJ_t4WA"},
+            {"label": "Bambuľka 3", "id": ""},
+            {"label": "Bambuľka 4", "id": ""},
+            {"label": "Bambuľka 5", "id": ""},
+            {"label": "Bambuľka 6", "id": ""},
         ]
 
         for ep in episody:
+
             li = xbmcgui.ListItem(label=ep["label"])
-            url = "plugin://plugin.video.youtube/play/?video_id=" + ep["id"]
-            li.setProperty('IsPlayable', 'true')
+
+            if ep["id"]:
+
+                url = "plugin://plugin.video.youtube/play/?video_id=" + ep["id"]
+                li.setProperty('IsPlayable', 'true')
+
+            else:
+
+                li.setInfo('video', {
+                    'title': ep["label"],
+                    'plot': 'Ešte sme nevysielali alebo nie je pridané.'
+                })
+
+                url = build_url({'action': 'oznam'})
+
             xbmcplugin.addDirectoryItem(handle, url, li, False)
-            
-        # 3. epizóda - Po kliknutí spustí akciu s oznamom hore v rohu obrazovky
-        li3 = xbmcgui.ListItem(label="Bambuľka 3")
-        li3.setInfo('video', {
-            'title': 'Bambuľka 3',
-            'plot': 'Ešte sme nevysielali alebo neni pridane.'
-        })
-        url3 = build_url({'action': 'bambulka3_oznam'})
-        xbmcplugin.addDirectoryItem(handle, url3, li3, False)
-            
+
         xbmcplugin.endOfDirectory(handle)
 
-    # --- SEKCIA FILMY ---
-    elif mode == 'filmy':
+    # ---------------- FILMY ----------------
+    elif mode == "filmy":
+
         li = xbmcgui.ListItem(label="[I]Filmy - Už čoskoro...[/I]")
-        # Musíme poslať aspoň základné kormidlo, aby Kodi vedelo kliknúť bez pádu
-        url_placeholder = build_url({'mode': 'filmy'})
-        xbmcplugin.addDirectoryItem(handle, url_placeholder, li, False)
+        url = build_url({'action': 'oznam'})
+        xbmcplugin.addDirectoryItem(handle, url, li, False)
+
         xbmcplugin.endOfDirectory(handle)
+
 
 if __name__ == '__main__':
     main()
+```
